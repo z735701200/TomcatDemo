1e435008d0b43af4bdb2a545c1f02557f7aa60a3
z735701200 <735701200@qq.com>
2018-08-11 13:18:43 +0800

    新建TomcatDemo项目

---
+++ src/cn/littletomcat/test/ExClass.java
@@ -0,0 +1,14 @@
+package cn.littletomcat.test;
+
+/**
+ * 测试类，被主容器动态调用
+ * @author 卫卫羊习习
+ */
+public class ExClass {
+    /**
+     * 测试方法
+     */
+    public void test() {
+        System.out.println("ExClass.test()方法被调用");
+    }
+}
